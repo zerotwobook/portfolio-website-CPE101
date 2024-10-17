c1844996bc0abe587c50f0a7ef4a0c7ad545a723
zerotwobook <135503171+zerotwobook@users.noreply.github.com>
2024-10-17 23:09:02 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,108 @@
+const menuIcon = document.querySelector('#menu-icon');
+const navLink = document.querySelector('.nav-link');
+const btn1 = document.getElementById('btnig');
+const btn2 = document.getElementById('btnfb');
+const btn3 = document.getElementById('btncn');
+const btn4 = document.getElementById('btnkid');
+const btn5 = document.getElementById('btnpr');
+const btn6 = document.getElementById('btnhg');
+const btn7 = document.getElementById('btnun');
+const btn8 = document.getElementById('btnhtml');
+const btn9 = document.getElementById('btncss');
+const btn10 = document.getElementById('btnjs');
+const btn11 = document.getElementById('btnpy');
+const btn12 = document.getElementById('btnvs');
+const btn13 = document.getElementById('btnig2');
+const btn14 = document.getElementById('btnfb2');
+const btn15 = document.getElementById('line');
+const btn16 = document.getElementById('btngmail');
+const btn17 = document.getElementById('btnimg');
+
+menuIcon.onclick = () => {
+    navLink.classList.toggle('active'); 
+};
+
+btn1.addEventListener('click', function() {
+    const url = "https://www.instagram.com/spy_bookxz__/";  
+    window.open(url, '_blank');  
+});
+
+btn2.addEventListener('click', function() {
+    const url = "https://www.facebook.com/supapanya.yathip/";  
+    window.open(url, '_blank');  
+});
+
+btn3.addEventListener('click', function() {
+    const url = "https://linktr.ee/supapanya";  
+    window.open(url, '_blank');  
+});
+
+btn4.addEventListener('click', function() {
+    const url = "https://www.veerasilp.ac.th/a";  
+    window.open(url, '_blank');  
+});
+
+btn5.addEventListener('click', function() {
+    const url = "https://www.drk.ac.th/";  
+    window.open(url, '_blank');  
+});
+
+btn6.addEventListener('click', function() {
+    const url = "https://www.visut.ac.th/";  
+    window.open(url, '_blank');  
+});
+
+btn7.addEventListener('click', function() {
+    const url = "https://www.kmutt.ac.th/a";  
+    window.open(url, '_blank');  
+});
+
+btn8.addEventListener('click', function() {
+    const url = "https://www.w3schools.com/html/default.asp";  
+    window.open(url, '_blank');  
+});
+
+btn9.addEventListener('click', function() {
+    const url = "https://www.w3schools.com/css/default.asp";  
+    window.open(url, '_blank');  
+});
+
+btn10.addEventListener('click', function() {
+    const url = "https://www.w3schools.com/js/default.asp";  
+    window.open(url, '_blank');  
+});
+
+btn11.addEventListener('click', function() {
+    const url = "https://www.w3schools.com/python/default.asp";  
+    window.open(url, '_blank');  
+});
+
+btn12.addEventListener('click', function() {
+    const url = "https://code.visualstudio.com/";  
+    window.open(url, '_blank');  
+});
+
+btn13.addEventListener('click', function() {
+    const url = "https://www.instagram.com/spy_bookxz__/";  
+    window.open(url, '_blank');  
+});
+
+btn14.addEventListener('click', function() {
+    const url = "https://www.facebook.com/supapanya.yathip/";  
+    window.open(url, '_blank');  
+});
+
+btn15.addEventListener('click', function() {
+    const url = "https://line.me/ti/p/pG9hIilkjU";  
+    window.open(url, '_blank');  
+});
+
+btn16.addEventListener('click', function() {
+    const url = "mailto:supapanyabook@gmail.com";  
+    window.open(url, '_blank');  
+});
+
+btn17.addEventListener('click', function() {
+    const url = "https://linktr.ee/supapanya";  
+    window.open(url, '_blank');  
+});
